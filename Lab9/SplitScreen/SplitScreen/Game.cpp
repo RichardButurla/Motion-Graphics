a7fc80f21ba85f06fb5cbb6eec48fe07c90f9793
RichardButurla <rj.buturla@gmail.com>
2023-02-28 15:25:30 +0000

    added split screen

---
+++ Lab9/SplitScreen/SplitScreen/Game.cpp
@@ -0,0 +1,273 @@
+#include "Game.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+/// <summary>
+/// default constructor
+/// setup the m_window properties
+/// load and setup the text 
+/// load and setup thne image
+/// </summary>
+Game::Game() :
+	m_window{ sf::VideoMode{ SCREEN_WIDTH, SCREEN_HEIGHT, 32U }, "Level Editor" },
+	m_exitGame{ false } //when true game will exit
+{
+	baseView = m_window.getView();
+	movingView = m_window.getView();
+	m_window.setView(movingView);
+
+	setupFontAndText(); // load font 
+	setupSprite(); // load texture
+	setupViews();
+}
+
+/// <summary>
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+/// </summary>
+Game::~Game()
+{
+}
+
+
+/// <summary>
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+/// </summary>
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if (sf::Event::Closed == newEvent.type) // window message
+		{
+			m_exitGame = true;
+		}
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+		if (sf::Event::MouseMoved == newEvent.type)
+		{
+			processMouseMove(newEvent);
+		}
+		if (sf::Event::MouseButtonPressed == newEvent.type)
+		{
+			processMousePress(newEvent);
+		}
+		if (sf::Event::MouseButtonReleased == newEvent.type)
+		{
+			processMouseRelease(newEvent);
+		}
+	}
+}
+
+
+/// <summary>
+/// deal with key presses from the user
+/// </summary>
+/// <param name="t_event">key press event</param>
+void Game::processKeys(sf::Event t_event)
+{
+	if (sf::Keyboard::Escape == t_event.key.code)
+	{
+		m_exitGame = true;
+	}
+	if (sf::Keyboard::Enter == t_event.key.code)
+	{
+
+	}
+	
+}
+
+
+void Game::processMousePress(sf::Event t_event)
+{
+	sf::Vector2i pixelPos = sf::Mouse::getPosition(m_window);
+	sf::Vector2f worldPos = m_window.mapPixelToCoords(pixelPos);
+
+	m_mousePressPos.x = worldPos.x;
+	m_mousePressPos.y = worldPos.y;
+
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		
+	}
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+	{
+		
+	}
+
+
+}
+
+void Game::processMouseRelease(sf::Event t_event)
+{
+	
+
+
+}
+
+void Game::processMouseMove(sf::Event t_event)
+{
+	sf::Vector2i pixelPos = sf::Mouse::getPosition(m_window);
+	sf::Vector2f worldPos = m_window.mapPixelToCoords(pixelPos);
+
+	m_mouseMovePos.x = worldPos.x;
+	m_mouseMovePos.y = worldPos.y;
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+	
+
+}
+
+/// <summary>
+/// draw the frame and then switch buffers
+/// </summary>
+void Game::render()
+{
+	right.setCenter({ 300,300 });
+	left.setCenter({ 600,500 });
+	m_window.clear(sf::Color::Black);
+
+
+	//minimap.setCenter(sf::Vector2f(player2.getPosition().x + (player1.getPosition().x) / 2, player2.getPosition().y + (player1.getPosition().y) / 2));
+
+
+
+
+	m_window.setView(left);
+	m_window.draw(map);
+	//m_window.draw(player1);
+	//m_window.draw(bulletPlayer1);
+	//m_window.draw(bulletPlayer2);
+	//m_window.draw(player2);
+	m_window.setView(right);
+	m_window.draw(map);
+	/*m_window.draw(bulletPlayer1);
+
+	m_window.draw(bulletPlayer2);
+	m_window.draw(player1);
+	m_window.draw(player2);*/
+
+
+
+	m_window.setView(fixed); // Draw 'GUI' elements with fixed positions
+
+	//m_window.draw(miniback);
+
+	//m_window.setView(minimap); // Draw minimap
+	//m_window.draw(map);
+
+	m_window.display();
+}
+
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::setupFontAndText()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+
+	/*m_hudText.setFont(m_ArialBlackfont);
+	m_hudText.setString(hudTexts[i]);
+	m_hudText.setStyle(sf::Text::Italic | sf::Text::Bold);
+	m_hudText.setPosition(70.0f, 20.0f);
+	m_hudText.setCharacterSize(30U);
+	m_hudText.setOutlineColor(sf::Color::Black);
+	m_hudText.setFillColor(hudTileColors[i]);
+	m_hudText.setOutlineThickness(3.0f);*/
+}
+
+/// <summary>
+/// load the texture and setup the sprite for the logo
+/// </summary>
+void Game::setupSprite()
+{
+	if (!texMap.loadFromFile("ASSETS\\IMAGES\\world.png"))
+	{
+		std::cout << "problem loading WORLD PNG" << std::endl;
+	}
+	if (!texLink.loadFromFile("ASSETS\\IMAGES\\link.png"))
+	{
+		std::cout << "problem loading link png" << std::endl;
+	}
+
+	map.setTexture(texMap);
+}
+
+void Game::setupViews()
+{
+	fixed = m_window.getView(); // The 'fixed' view will never change
+	standard = fixed; // The 'standard' view will be the one that gets always displayed
+
+	size = 200; // The 'minimap' view will show a smaller picture of the map
+	minimap = sf::View(sf::FloatRect(standard.getCenter().x, standard.getCenter().y, static_cast<float>(size), static_cast<float>(m_window.getSize().y * size / m_window.getSize().x)));
+	minimap.setViewport(sf::FloatRect(1.f - static_cast<float>(minimap.getSize().x) / m_window.getSize().x - 0.02f, 1.f - static_cast<float>(minimap.getSize().y) / m_window.getSize().y - 0.02f,
+		static_cast<float>(minimap.getSize().x) / m_window.getSize().x, static_cast<float>(minimap.getSize().y) / m_window.getSize().y));
+	minimap.zoom(4.f);
+
+	// The 'left' and the 'right' view will be used for splitscreen displays
+	left = sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(m_window.getSize().x / 2), static_cast<float>(m_window.getSize().y)));
+	left.setViewport(sf::FloatRect(0.f, 0.f, 0.5, 1.f));
+	right = sf::View(sf::FloatRect(0.f, 0.f, static_cast<float>(m_window.getSize().x / 2), static_cast<float>(m_window.getSize().y)));
+	right.setViewport(sf::FloatRect(0.5, 0.f, 0.5, 1.f));
+
+	// We want to draw a rectangle behind the minimap
+	miniback.setPosition(minimap.getViewport().left * m_window.getSize().x - 5, minimap.getViewport().top * m_window.getSize().y - 5);
+	miniback.setSize(sf::Vector2f(minimap.getViewport().width * m_window.getSize().x + 10, minimap.getViewport().height * m_window.getSize().y + 10));
+	miniback.setFillColor(sf::Color(160, 8, 8));
+
+
+
+
+
+
+
+	/*left.setCenter(player1.getPosition());
+	right.setCenter(player2.getPosition());*/
+}
---
+++ Lab9/SplitScreen/SplitScreen/Game.h
@@ -0,0 +1,68 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+	static const int SCREEN_WIDTH = 800;
+	static const int SCREEN_HEIGHT = 600;
+
+	static const int MAX_COLLUMS = 25;
+	static const int MAX_ROWS = 17;
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void processMousePress(sf::Event t_event);
+	void processMouseRelease(sf::Event t_event);
+	void processMouseMove(sf::Event t_event);
+
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void setupFontAndText();
+	void setupSprite();
+	void setupViews();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::View baseView;
+	sf::View movingView;
+	sf::Font m_ArialBlackfont; // font used by message
+
+	sf::Texture texMap; // Create the world texture and sprite
+	sf::Texture texLink; // Create the link texture and sprite
+
+	sf::Sprite map;
+
+
+	sf::View fixed;
+	sf::View standard;
+	sf::View minimap;
+	unsigned int size = 200; // The 'minimap' view will show a smaller picture of the map
+	
+
+	// The 'left' and the 'right' view will be used for splitscreen displays
+	sf::View left;
+	sf::View right;
+	sf::RectangleShape miniback; // We want to draw a rectangle behind the minimap
+
+	//Mouse
+	sf::Vector2f m_mousePressPos;
+	sf::Vector2f m_mouseMovePos;
+
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
